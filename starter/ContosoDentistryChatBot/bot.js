87b7e7c88aa9c907d8b92daf6628330d9c14ff4c
gmj06 <gowrimj2006@gmail.com>
2022-06-13 13:07:22 -0700

    Implemented QnA Maker

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -14,7 +14,7 @@ class DentaBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
-        //this.QnAMaker = new QnAMaker()
+        this.QnAMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions)
        
         // create a DentistScheduler connector
       
@@ -24,7 +24,17 @@ class DentaBot extends ActivityHandler {
         this.onMessage(async (context, next) => {
             // send user input to QnA Maker and collect the response in a variable
             // don't forget to use the 'await' keyword
-          
+            const qnaResults = await this.QnAMaker.getAnswers(context);
+            // If an answer was received from QnA Maker, send the answer back to the user.
+            if (qnaResults[0]) {
+                console.log(qnaResults[0])
+                await context.sendActivity(`${qnaResults[0].answer}`);
+            }
+            else {
+                // If no answers were returned from QnA Maker, reply with help.
+                await context.sendActivity(`I'm not sure I found an answer to your question`
+                + `You can ask me questions about dental office appointment scheduling or FAQs like "I don't have insurance. Can I still be seen?"`);
+            }
             // send user input to IntentRecognizer and collect the response in a variable
             // don't forget 'await'
                      
@@ -44,7 +54,7 @@ class DentaBot extends ActivityHandler {
         this.onMembersAdded(async (context, next) => {
         const membersAdded = context.activity.membersAdded;
         //write a custom greeting
-        const welcomeText = '';
+        const welcomeText = 'Welcome to Dental Office Virtual Assistant';
         for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
             if (membersAdded[cnt].id !== context.activity.recipient.id) {
                 await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
